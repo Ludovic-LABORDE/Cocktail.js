69ede5ac824f5abd25f0d55d1879066daf03daeb
Ludovic-LABORDE <laborde.contact1@gmail.com>
2023-05-19 16:12:05 +0200

    Added new img and error on search submit

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,5 +1,6 @@
 async function ApiFindElements (){
-    const url = 'https://api.api-ninjas.com/v1/cocktail?name=rhum';
+    const url = 'https://api.api-ninjas.com/v1/cocktail?name=' + randomCocktail();
+    console.log(url)
     const options = {
         method: 'GET',
         headers: { 'X-Api-Key': 'riYvZgGkC76ZcX+fWUCh9A==Kwu7qtxEbP8L6NuT'},
@@ -9,15 +10,17 @@ async function ApiFindElements (){
     try {
         const response = await fetch(url, options);
         const result = await response.json();
-        // insertInDom(result);
-        // console.log(result);
+        insertInDom(result)
     } catch (error) {
         console.error(error);
+
     }
 }
 function SearchElements (){
     searchInput = document.querySelector('form');
     searchInput.addEventListener('submit', handleFormSubmit);
+    searchElement = document.querySelector("#search-wrapper");
+    searchElement.classList.remove(".open")
 }
 async function handleFormSubmit(event){
     event.preventDefault();
@@ -29,28 +32,56 @@ async function handleFormSubmit(event){
         headers: { 'X-Api-Key': 'riYvZgGkC76ZcX+fWUCh9A==Kwu7qtxEbP8L6NuT'},
         contentType: 'application/json',
     };
-
+    
     try {
         const response = await fetch(url, options);
         const result = await response.json();
-        insertInDom(result);
-        location.href = "#the-menu"
-    } catch (error) {
-        console.error(error);
+        console.log(result.length)
+        if(result.length === 0){
+            SearchError(result);  
+        }else{
+            location.href = "#the-menu";
+            return insertAllInDom(result);
+        }
+        
+    } catch {
+        SearchError();
+        console.error("Not find your request");
     }
 
 }
-function insertInDom(data){
-    console.log(data)
+function SearchError(result){
+    errorElement = document.querySelector("#error-form");
+    // if(){
+    //     errorElement.textContent = "Sorry, we did not find your request";
+    //     errorElement.removeAttribute('hidden');
+    //     setTimeout(() => {
+    //         errorElement.setAttribute('hidden', "");
+    
+    //     }, 3000)
+    // }
+    if(result.length === 0){
+        errorElement.textContent = "Sorry, we did not find your request";
+        errorElement.removeAttribute('hidden');
+        setTimeout(() => {
+            errorElement.setAttribute('hidden', "");
+    
+        }, 3000)
+    }else{
+        return;
+    }
+}
+function insertAllInDom(data){
+    // console.log(data)
     if (data.lenght === 0) {
-
         return console.error(error)
 
     } else {
-
+        
+        removeInDom()
         data.forEach(key => {
             containerElement = document.querySelector("#container-cocktail")
-            searchElement = document.querySelector('#search-wrapper')
+            searchElement = document.querySelector('#search-wrapper');
     
             nameElement             = document.createElement('h3');
             ingredientElement       = document.createElement('h4');
@@ -64,24 +95,129 @@ function insertInDom(data){
             containerElement.appendChild(instructionTitleElement);
             containerElement.appendChild(instructionElement);
     
-            UlElement.classList.add('list-unstyled')
-            searchElement.classList.remove('open')
+            nameElement.classList.add('theme-accent-color');
+            UlElement.classList.add('list-unstyled');
+            searchElement.classList.remove('open');
     
-            nameElement.textContent = key.name
+            nameElement.textContent = key.name;
             instructionElement.textContent = key.instructions;
-            ingredientElement.textContent = "Ingrédient"
-            instructionTitleElement.textContent = "Instructions"
+            ingredientElement.textContent = "Ingrédients";
+            instructionTitleElement.textContent = "Instructions";
             
             key.ingredients.forEach(ingredients => {
                 lElement = document.createElement('li');
                 UlElement.appendChild(lElement);
                 lElement.textContent = ingredients;
-                
+
             });
-            
         });
 
     }
 }
-ApiFindElements()
-SearchElements ()
+function insertInDom(data){
+    containerElement = document.querySelector("#container-cocktail")
+    searchElement = document.querySelector('#search-wrapper');
+
+    nameElement             = document.createElement('h3');
+    ingredientElement       = document.createElement('h4');
+    UlElement               = document.createElement('ul');
+    instructionTitleElement = document.createElement('h4');
+    instructionElement      = document.createElement('p');
+
+    containerElement.appendChild(nameElement);
+    containerElement.appendChild(ingredientElement);
+    UlElement = containerElement.appendChild(UlElement);
+    containerElement.appendChild(instructionTitleElement);
+    containerElement.appendChild(instructionElement);
+
+    nameElement.classList.add('theme-accent-color', 'fs-1');
+    UlElement.classList.add('list-unstyled');
+    searchElement.classList.remove('open');
+
+    nameElement.textContent = data[0].name;
+    instructionElement.textContent = data[0].instructions;
+    ingredientElement.textContent = "Ingrédients";
+    instructionTitleElement.textContent = "Instructions";
+    console.log(data)
+    data[0].ingredients.forEach(ingredients => {
+        lElement = document.createElement('li');
+        UlElement.appendChild(lElement);
+        lElement.textContent = ingredients;
+
+    });
+}
+function removeInDom(){
+    containerElement = document.querySelector("#container-cocktail");
+    while(containerElement.firstChild){;
+        containerElement.removeChild(containerElement.lastChild);
+    };
+
+}
+function randomCocktail(){
+    const cocktailArray = [
+        "Adios Motherfucker",
+        "Alaska",
+        "Alexander",
+        "Americano",
+        "B-52",
+        "Batida",
+        "Bijou",
+        "Black Russian",,
+        "Bloody Mary",
+        "Blue Lagoon",
+        "Bronx",
+        "Caesar",
+        "Columbia",
+        "Cosmopolitan",
+        "Daiquiri",
+        "Embuscade",
+        "French Connection",
+        "Gibson",
+        "Grasshopper",
+        "Grog",
+        "Harvey Wallbanger",
+        "Horse's Neck",
+        "Irish Coffee",
+        "Jack Rose",
+        "Karaoudjan",
+        "Mai Tai",
+        "Manhattan",
+        "Margarita",
+        "Martini",
+        "Mimosa",
+        "Mojito",
+        "Monkey gland",
+        "Negroni",
+        "Nixon",
+        "Old fashioned",
+        "Paradise",
+        "Piña colada",
+        "Pink Lady",
+        "Pisco sour",
+        "Porto Flip",
+        "Presidente",
+        "Punch",
+        "Rose",
+        "Sangria",
+        "Sake Bomb",
+        "Sazerac",
+        "Screwdriver",
+        "Stinger",
+        "Suissesse",
+        "Tamagozake",
+        "Tango",
+        "Tequila Sunrise",
+        "Tinto de Verano",
+        "Vesper",
+        "Vodka",
+        "Whisky",
+        "White Russian",
+        "Zombie",
+    ]
+    const randomNumber = Math.floor(Math.random() * 56);
+    const randomCoktail = cocktailArray[randomNumber];
+    console.log(randomCoktail)
+    return randomCoktail;
+}
+ApiFindElements();
+SearchElements ();
